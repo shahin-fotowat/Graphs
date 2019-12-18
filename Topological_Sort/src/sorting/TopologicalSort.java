ece03a1ec3649b8fa212b70b938863957d8a7917
Virtual6 <sfototwa@uci.edu>
2019-12-18 00:49:19 -0800

    Made some changes to TPS

--- Topological_Sort/src/sorting/TopologicalSort.java
+++ Topological_Sort/src/sorting/TopologicalSort.java
@@ -6,28 +6,29 @@ import node.GraphNode;
 public class TopologicalSort {
 	ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
 
-	
 	//Constructor
 	public TopologicalSort(ArrayList<GraphNode> nodeList) {
 		this.nodeList = nodeList;
 	}
 
-	
 	void topologicalSort() {
-		Stack<GraphNode>stack = new Stack<>();
-		for (GraphNode node : nodeList) { // if a node is unvisited then run topologicalSort on it
+		// Create the containing the vertices that are added 
+		Stack<GraphNode> stack = new Stack<>();
+		// if a node is unvisited then run topologicalSort on it
+		for (GraphNode node : nodeList) {
 			if (!node.isVisited())
 				topologicalVisit(node,stack);
 		}
-		while(!stack.isEmpty()){
-			System.out.print(stack.pop().getName()+" ");
+		// After finishing the sort pop each vertex from the stack and print it
+		while(!stack.isEmpty()) {
+			System.out.print(stack.pop().getName() + " ");
 		}
 	}
-
 	
 	// Topological visit by a source node
 	void topologicalVisit(GraphNode node, Stack<GraphNode> stack) {
-		for (GraphNode neighbor : node.getNeighbors()){	//if neighbor is not visited then recursive call to it
+		// if neighbor is not visited then recursive call to it
+		for (GraphNode neighbor : node.getNeighbors()){	
 			if(!neighbor.isVisited()){
 				topologicalVisit(neighbor,stack);
 			}
@@ -35,11 +36,10 @@ public class TopologicalSort {
 		node.setVisited(true);
 		stack.push(node);
 	} // end of method
-
 	
 	// add a directed edge between two nodes
 	public void addDirectedEdge(int i, int j) {
-		GraphNode first = nodeList.get(i - 1);
+		GraphNode first  = nodeList.get(i - 1);
 		GraphNode second = nodeList.get(j - 1);
 		first.getNeighbors().add(second);
 	} // end of method
